59cb3232041c2ccc1c45454c5cf9c2e539cfb00d
Martin Szabo <martin.szabo333@gmail.com>
2024-03-02 12:11:27 +0100

    Rozpracovanie query 3 endpointu zadania 2

---
+++ Zadanie_2_3.sql
@@ -0,0 +1,10 @@
+-- Ziskaj count postov tagnutych podla tagu na vstupe
+-- Podel to overall poctom postov
+
+-- SELECT *
+-- FROM tags
+-- INNER JOIN post_tags ON tags.id = post_tags.tag_id
+-- -- Teraz vykonat aritmetiku pre tag napr. linux
+
+SELECT *
+FROM tags
