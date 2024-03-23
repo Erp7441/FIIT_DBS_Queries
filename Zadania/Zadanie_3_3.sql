0464d67d655be396d209691795339b7cb5a538cf
Martin Szabo <martin.szabo333@gmail.com>
2024-03-23 01:00:21 +0100

    forgotten comment

--- Zadania/Zadanie_3_3.sql
+++ Zadania/Zadanie_3_3.sql
@@ -4,7 +4,7 @@ FROM (
 		c.id AS id,
 		displayname, body, text,
 		c.score AS score,
-		(ROW_NUMBER() OVER (ORDER BY c.id)) % 2 + 1 AS position,
+		(ROW_NUMBER() OVER (ORDER BY c.id)) % 2 + 1 AS position, -- Parameter
 		TO_CHAR(p.creationdate AT TIME ZONE 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS.MSOF:TZM') AS creationdate
 	FROM comments c
 	JOIN posts p ON c.postid = p.id
