df060e87be04f766ae2cc79ec70d2c872554fc2a
Martin Szabo <martin.szabo333@gmail.com>
2024-04-21 21:35:40 +0200

    Struktura 2

--- Zadania/Zadanie_5_all.sql
+++ Zadania/Zadanie_5_all.sql
@@ -224,6 +224,23 @@ BEFORE INSERT OR UPDATE ON showcased_exemplars
 FOR EACH ROW EXECUTE FUNCTION validate_showcased_dates();
 
 
+-- Tento trigger zabezpeci, ze datum pozicania je skor ako datum vratenia
+CREATE OR REPLACE FUNCTION validate_borrow_dates()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.borrowdate >= NEW.returndate THEN
+		RAISE EXCEPTION 'BorrowDate must be earlier than ReturnDate.'; -- Kontrola platnosti datumov
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger, ktory sa spusti pred vlozenim alebo aktualizaciou zaznamu v tabulke borrows
+CREATE TRIGGER trigger_validate_borrow_dates
+BEFORE INSERT OR UPDATE ON borrows
+FOR EACH ROW EXECUTE FUNCTION validate_borrow_dates();
+
+
 ------------------------------------------------------------------------------
 -- Zmazanie exemplara
 ------------------------------------------------------------------------------
@@ -268,21 +285,6 @@ FOR EACH ROW EXECUTE FUNCTION prevent_decommission_status_change();
 ------------------------------------------------------------------------------
 -- Vypozicky
 ------------------------------------------------------------------------------
--- Tento trigger zabezpeci, ze datum pozicania je skor ako datum vratenia
-CREATE OR REPLACE FUNCTION validate_borrow_dates()
-RETURNS TRIGGER AS $$
-BEGIN
-	IF NEW.borrowdate >= NEW.returndate THEN
-		RAISE EXCEPTION 'BorrowDate must be earlier than ReturnDate.'; -- Kontrola platnosti datumov
-	END IF;
-	RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
--- Trigger, ktory sa spusti pred vlozenim alebo aktualizaciou zaznamu v tabulke borrows
-CREATE TRIGGER trigger_validate_borrow_dates
-BEFORE INSERT OR UPDATE ON borrows
-FOR EACH ROW EXECUTE FUNCTION validate_borrow_dates();
 
 
 
